1b892a0db8f3eb4608ff1eb8535549a7ecf82093
Anibal <jovomilla@gmail.com>
2017-09-08 16:36:59 -0500

    Primmer Commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+		$posts = Post::published()->paginate();
+
+    	return view('welcome', compact('posts'));
+    }
+
+
+
+
+}
